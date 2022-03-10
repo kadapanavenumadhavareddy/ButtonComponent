9aac786bc4e1f1ae616bff24e4c5c5c5874b314a
kadapanavenumadhavareddy <venumadhavareddy8@gmail.com>
2022-03-10 16:33:58 +0530

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <div>
+      <h1>Button Default Stage</h1>
+      <ButtonComponent />
+    </div>
+    <div>
+      <h1>Button Variant = outline</h1>
+      <ButtonComponent variant="outline" />
+    </div>
+    <div>
+      <h1>Button Variant = text</h1>
+      <ButtonComponent variant="text" />
+    </div>
+    <div>
+      <h1>Button DisabledShadow</h1>
+      <ButtonComponent disabledShadow />
+    </div>
+
+    <div>
+      <h1>Button Disabled</h1>
+      <ButtonComponent disabled />
+    </div>
+    <div>
+      <h1>Button Disabled at variant = text</h1>
+      <ButtonComponent disabled variant="text" />
+    </div>
+
+    <div>
+      <h1>Button with starting icon</h1>
+      <ButtonComponent startIcon="local_grocery_store" />
+    </div>
+    <div>
+      <h1>Button with ending icon</h1>
+      <ButtonComponent endIcon="local_grocery_store" />
+    </div>
+
+    <div>
+      <h1>Button size = sm</h1>
+      <ButtonComponent size="sm" />
+    </div>
+    <div>
+      <h1>Button size = md</h1>
+      <ButtonComponent size="md" />
+    </div>
+    <div>
+      <h1>Button size = lg</h1>
+      <ButtonComponent size="lg" />
+    </div>
+
+    <div>
+      <h1>Button color = default</h1>
+      <ButtonComponent color="default" />
+    </div>
+    <div>
+      <h1>Button color = primary</h1>
+      <ButtonComponent color="primary" />
+    </div>
+    <div>
+      <h1>Button color = secondary</h1>
+      <ButtonComponent color="secondary">secondary</ButtonComponent>
+    </div>
+    <div>
+      <h1>Button color = danger</h1>
+      <ButtonComponent color="danger" />
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ButtonComponent from "./components/ButtonComponent.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ButtonComponent,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+section {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+div {
+  margin: 20px;
 }
 </style>
---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,139 @@
+<template>
+  <button :class="[variant, size, color]">
+    <span class="material-icons starticon" v-if="$props.startIcon">
+      {{ startIcon }} </span
+    ><slot>Default</slot>
+    <span class="material-icons endicon" v-if="$props.endIcon">
+      {{ endIcon }}
+    </span>
+  </button>
+</template>
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    size: {
+      type: String,
+      default: "md",
+      validator(value) {
+        return ["sm", "md", "lg"].includes(value);
+      },
+    },
+    variant: {
+      type: String,
+      validator(value) {
+        return ["outline", "text"].includes(value);
+      },
+    },
+    color: {
+      type: String,
+      validator(value) {
+        return ["primary", "secondary", "default", "danger"].includes(value);
+      },
+    },
+    startIcon: {
+      type: String,
+    },
+    endIcon: {
+      type: String,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+/**default stage */
+button {
+  height: max-content;
+  width: max-content;
+  padding: 8px 16px;
+  background-color: #e0e0e0;
+  font-family: "Noto Sans", sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20.27px;
+  border: none;
+  border-radius: 6px;
+  color: #3f3f3f;
+  box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+}
+button:hover,
+button:focus {
+  background-color: #aeaeae;
+}
+/**variant styles */
+.outline {
+  border: 1px solid #3d5afe;
+}
+.text,
+.outline {
+  color: #3d5afe;
+  background-color: white;
+  box-shadow: none;
+}
+.text:hover,
+.text:focus,
+.outline:hover,
+.outline:focus {
+  background-color: rgba(41, 98, 255, 0.1);
+}
+/**differnt sizes */
+.sm {
+  padding: 6px 12px;
+}
+.lg {
+  padding: 11px 22px;
+}
+/**colors primary,secondary,danger,default */
+.primary {
+  background-color: #2962ff;
+  box-shadow: 0px 2px 3px rgba(41, 98, 255, 0.2);
+}
+.secondary {
+  background-color: #455a64;
+  box-shadow: 0px 2px 3px rgba(69, 90, 100, 0.2);
+}
+.danger {
+  background-color: #d32f2f;
+
+  box-shadow: 0px 2px 3px rgba(211, 47, 47, 0.2);
+}
+.primary,
+.secondary,
+.danger {
+  color: #ffffff;
+}
+.primary:hover,
+.primary:focus {
+  background-color: #0039cb;
+}
+.secondary:hover,
+.secondary:focus {
+  background-color: #1c313a;
+}
+.danger:hover,
+.danger:focus {
+  background-color: #9a0007;
+}
+/**styling for icons */
+.material-icons {
+  font-size: 14px;
+  line-height: 20.27px;
+}
+.starticon {
+  padding: 0px 10px 0px 0px;
+}
+.endicon {
+  padding: 0px 0px 0px 10px;
+}
+
+/**disabled and disabledShadow */
+button[disabled] {
+  color: #9e9e9e;
+}
+button[disabledShadow] {
+  box-shadow: none;
+}
+</style>
